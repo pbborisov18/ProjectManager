66e4d817bed4c0f55533adf83bf97c26998058ca
pbborisov18 <pbborisov18@codingburgas.bg>
2023-04-09 14:03:26 +0300

    Create structure for the back end

---
+++ src/test/java/com/company/projectManager/StarterApplicationTests.java
@@ -0,0 +1,11 @@
+package com.company.projectManager;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+    void contextLoads() {
+    }
+
+}
