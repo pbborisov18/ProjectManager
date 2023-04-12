38cc23c4549f7a27fe323049674db9a02087548b
pbborisov18 <pbborisov18@codingburgas.bg>
2023-04-12 22:55:03 +0300

    Change the query to a custom one

--- src/main/java/com/company/projectManager/repositories/InviteRepository.java
+++ src/main/java/com/company/projectManager/repositories/InviteRepository.java
@@ -1,10 +1,14 @@
 package com.company.projectManager.repositories;
 
 import com.company.projectManager.models.Invite;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface InviteRepository extends CrudRepository<Invite, Long>{
-    List<Invite> findByReceiverId(Long receiverId);
+    @Query("SELECT i " +
+            "FROM Invite i " +
+            "WHERE i.receiver.id = :receiverId AND i.state = 'PENDING'")
+    List<Invite> findByReceiverIdAndStateNotIn(Long receiverId);
 }
