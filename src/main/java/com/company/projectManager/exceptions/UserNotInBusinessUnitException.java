516548071579872930324c7a602f767047e6e55d
pbborisov18 <pbborisov18@codingburgas.bg>
2023-04-12 22:57:55 +0300

    Added needed exception

---
+++ src/main/java/com/company/projectManager/exceptions/UserNotInBusinessUnitException.java
@@ -0,0 +1,8 @@
+package com.company.projectManager.exceptions;
+
+public class UserNotInBusinessUnitException extends Exception {
+
+    public UserNotInBusinessUnitException(String message) {
+        super(message);
+    }
+}
