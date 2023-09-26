09edcfccb9995f00846b211696f07f9f4cf15e8a
pbborisov18 <pbborisov18@codingburgas.bg>
2023-09-27 00:43:04 +0300

    Fixed typo

--- Client/my-app/src/lib/components/TeamComponent.svelte
+++ Client/my-app/src/lib/components/TeamComponent.svelte
@@ -267,7 +267,7 @@
 <Modal bind:open={deletePopup} size="xs" autoclose>
     <div class="text-center">
         <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        Are you sure you want to leave the team?
+        Are you sure you want to delete the team?
         <Button color="red" class="mr-2" on:click={deleteBU}>Yes</Button>
         <Button color='alternative'>No</Button>
     </div>
