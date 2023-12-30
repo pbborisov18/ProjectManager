e1a0ae4f2f23bcf7ce060b12f6db5252d6696dd9
pbborisov18 <PetarBorisovDev@gmail.com>
2023-12-30 17:13:24 +0200

    Fix up the header
    - Clean up the fetch methods
    - Clean up syntax

--- Client/my-app/src/lib/components/Header.svelte
+++ Client/my-app/src/lib/components/Header.svelte
@@ -15,7 +15,6 @@
         } else {
             goto("/");
         }
-
     }
 
     function redirectToInvitesPage(){
@@ -23,23 +22,21 @@
     }
 
     function logout() {
-        loggedIn.set("false");
         fetch('http://localhost:8080/logout', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             credentials: "include"
-        })
-            .then(response => {
+        }).then(response => {
                 if (response.status === 200) {
                     loggedIn.set("");
                     goto("/login");
                 } else {
-                    throw Error("Logout failed");
+                    // notification
                 }
-            }).catch(error => {
-                alert(error);
+        }).catch(error => {
+            // Server died or something
         })
 
     }
@@ -51,7 +48,6 @@
     function redirectToRegister(){
         goto("/sign-up");
     }
-
 
 </script>
 
@@ -78,31 +74,29 @@
         <div class="logged-in-section">
             <img class="clickable" src="{inviteIcon}" alt="invite button" draggable="false" on:click={redirectToInvitesPage}>
 
-            <div class="test clickable not-selectable" >
-                <span>{$userEmail}</span>
+            <div class="emailDivBox not-selectable" >
+                <span class="clickable">{$userEmail}</span>
+                <Dropdown >
+                    <DropdownItem on:click={logout}>Logout</DropdownItem>
+                </Dropdown>
             </div>
-            <Dropdown >
-                <DropdownItem on:click={logout}>Logout</DropdownItem>
-            </Dropdown>
         </div>
     {/if}
 </header>
 
 <style lang="scss">
 
-
     header {
         font-family: sans-serif;
         display: flex;
         align-items: center;
-        box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 1px;
+        box-shadow: rgba(0, 0, 0, 0.12) 0 1px 1px;
         background-color: #eae9ea;
     }
 
     img {
         width: 50px;
     }
-
 
     span {
         font-family: Bahnschrift, monospace;
@@ -120,8 +114,8 @@
     }
 
     .logo{
-      margin-left: 1vw;
-      margin-right: 1vw;
+        margin-left: 1vw;
+        margin-right: 1vw;
     }
 
     .landingPage-section{
@@ -136,37 +130,35 @@
     }
 
     .logged-in-section {
-      display: flex;
-      align-items: center;
-      margin-left: auto;
-      height: 7vh;
-
-      img {
-        width: 30px;
-        height: 30px;
-        margin-right: 3vw;
-      }
-
-      span {
-        font-size: 14px;
-        font-weight: bold;
-        color: #333333;
-        margin-right: 30px;
-        font-family: Bahnschrift, monospace;
-        flex: 1;
-        text-align: center;
-      }
-
-      .test{
-        height: 7vh;
-        margin-right: 3vw;
         display: flex;
-        justify-content: center;
         align-items: center;
-        text-align: center;
-      }
+        margin-left: auto;
+        height: 7vh;
+
+        img {
+            width: 30px;
+            height: 30px;
+            margin-right: 3vw;
+        }
+
+        span {
+            font-size: 14px;
+            font-weight: bold;
+            color: #333333;
+            margin-right: 30px;
+            font-family: Bahnschrift, monospace;
+            flex: 1;
+            text-align: center;
+        }
+
+        .emailDivBox{
+            height: 7vh;
+            margin-right: 3vw;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
     }
-
-
 
 </style>
